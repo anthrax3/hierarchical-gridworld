18b1654dca1857f395270ef4d003f2fec75b28aa
Paul Christiano <paulfchristiano@gmail.com>
2017-07-10 23:06:51 -0700

    Initial commit

---
+++ env.py
@@ -0,0 +1,216 @@
+import pyparsing as pp
+from utils import unweave, areinstances, clear_screen, interleave
+from message import Message, Pointer, Channel, Referent, addressed_message, World
+from world import move_person, move_gaze, look, empty_world
+
+class Env(object):
+    def __init__(self, messages=(), actions=()):
+        self.messages = messages
+        self.actions = actions
+        all_args = []
+        for m in messages:
+            all_args.extend(m.args)
+        self.args = tuple(all_args)
+        assert self.well_formed()
+
+    def well_formed(self):
+        return (
+            areinstances(self.args, Referent),
+            areinstances(self.messages, Message),
+        )
+
+    def get_obs(self):
+        last_arg = 0
+        message_lines = []
+        action_lines = []
+        for m in self.messages:
+            new_last_arg = last_arg + m.size
+            s = m.format_with_indices(range(last_arg, new_last_arg))
+            message_lines.append(">>> {}\n".format(s))
+            last_arg = new_last_arg
+        for a in self.actions:
+            action_lines.append("<<< {}".format(a))
+        return "\n".join(interleave(message_lines, action_lines))
+
+    def step(self, act):
+        command = parse_command(act)
+        new_env = self.add_action(act)
+        message, retval = command.execute(new_env)
+        if message is not None:
+            new_env = new_env.add_message(message)
+        obs = new_env.get_obs()
+        return obs, retval, new_env
+
+    def add_message(self, m):
+        return Env(messages=self.messages + (m,), actions=self.actions)
+
+    def add_action(self, a):
+        return Env(messages=self.messages, actions=self.actions + (a,))
+
+
+def main(env=Env((Message("[] is an empty world", World(empty_world(5, 5))),))):
+    obs = env.get_obs()
+    while True:
+        act = get_action(obs)
+        obs, retval, env = env.step(act)
+        if retval is not None:
+            return retval, env
+
+def get_action(obs):
+    clear_screen()
+    print(obs)
+    return input("<<< ")
+
+
+#----commands
+
+class Command(object):
+
+    def execute(self, env):
+        raise NotImplemented()
+
+class Ask(Command):
+
+    def __init__(self, message, recipient=None):
+        self.message = message
+        self.recipient_pointer = recipient
+
+    def execute(self, env):
+        message = addressed_message(self.message.instantiate(env.args), env)
+        if self.recipient_pointer is None:
+            new_env = Env()
+        else:
+            new_env = self.recipient_pointer.instantiate(env.args).env
+        new_env = new_env.add_message(message)
+        response, new_env = main(new_env)
+        return addressed_message(response, new_env), None
+
+class View(Command):
+
+    def __init__(self, message):
+        self.message = message
+
+    def execute(self, env):
+        return self.message.instantiate(env.args), None
+
+class Return(Command):
+
+    def __init__(self, message):
+        self.message = message
+
+    def execute(self, env):
+        return None, self.message.instantiate(env.args)
+
+class Reflect(Command):
+
+    def execute(self, env):
+        return Message("you are []", Channel(env)), None
+
+class MalformedCommand(Command):
+
+    def execute(self, env):
+        return Message("the valid commands are 'reply', 'ask', 'reflect', 'view', and 'ask@N'"), None
+
+class Move(Command):
+
+    def __init__(self, world, direction):
+        self.world_pointer = world
+        self.direction = direction
+
+    def execute(self, env):
+        world = self.world_pointer.instantiate(env.args).world
+        new_world, moved = move_person(world, self.direction)
+        result = Message("the result is []", World(new_world)) if moved else Message("you can't move")
+        return result, None
+
+class Gaze(Command):
+
+    def __init__(self, world, direction):
+        self.world_pointer = world
+        self.direction = direction
+
+    def execute(self, env):
+        world = self.world_pointer.instantiate(env.args).world
+        new_world, moved = move_gaze(world, self.direction)
+        result = Message("the result is []", World(new_world)) if moved else Message("you can't move")
+        return result, None
+
+class Look(Command):
+
+    def __init__(self, world):
+        self.world_pointer = world
+
+    def execute(self, env):
+        world = self.world_pointer.instantiate(env.args).world
+        result = look(world)
+        return Message("you see {}".format(result)), None
+
+#----parsing
+
+def parse_command(s):
+    try:
+        return command.parseString(s, parseAll=True)[0]
+    except pp.ParseException:
+        return MalformedCommand()
+
+def parse_message(s):
+    try:
+        return message.parseString(s, parseAll=True)[0]
+    except pp.ParseException:
+        return Message("<<malformed message>>")
+
+def raw(s):
+    return pp.Literal(s).suppress()
+def options(*xs):
+    result = pp.Literal(xs[0])
+    for x in xs[1:]:
+        result = result ^ pp.Literal(x)
+    return result
+
+number = pp.Word("0123456789").setParseAction(lambda t : int(t[0]))
+prose = pp.Word(" ,!?+-/*.;:_<>=&%{}[]\'\"" + pp.alphas).leaveWhitespace()
+
+agent_referent = (raw("@")+ number).leaveWhitespace()
+agent_referent.setParseAction(lambda x : Pointer(x[0], Channel))
+
+message_referent = (raw("#") + number).leaveWhitespace()
+message_referent.setParseAction(lambda x : Pointer(x[0], Message))
+
+world_referent = (raw("$") + number).leaveWhitespace()
+world_referent.setParseAction(lambda xs : Pointer(xs[0], World))
+
+message = pp.Forward()
+submessage = raw("(") + message + raw(")")
+argument = submessage | agent_referent | message_referent | world_referent
+literal_message = (
+        pp.Optional(prose, default="") +
+        pp.ZeroOrMore(argument + pp.Optional(prose, default=""))
+    ).setParseAction(lambda xs : Message(tuple(unweave(xs)[0]), *unweave(xs)[1]))
+message << (message_referent ^ literal_message)
+
+target_modifier = raw("@")+number
+target_modifier.setParseAction(lambda xs : ("recipient", Pointer(xs[0], type=Channel)))
+
+ask_modifiers = pp.ZeroOrMore(target_modifier)
+ask_modifiers.setParseAction(lambda xs : dict(list(xs)))
+
+ask_command = (raw("ask")) + ask_modifiers + message
+ask_command.setParseAction(lambda xs : Ask(xs[1], **xs[0]))
+
+reply_command = (raw("reply") | raw("return")) + message
+reply_command.setParseAction(lambda xs : Return(xs[0]))
+
+reflect_command = raw("reflect")
+reflect_command.setParseAction(lambda xs : Reflect())
+
+view_command = raw("view") + message
+view_command.setParseAction(lambda xs : View(xs[0]))
+
+move_command = raw("move") + pp.Empty() + world_referent + options("left", "up", "right", "down")
+move_command.setParseAction(lambda xs : Move(xs[0], xs[1]))
+gaze_command = raw("gaze") + pp.Empty() + world_referent + options("left", "up", "right", "down")
+gaze_command.setParseAction(lambda xs : Gaze(xs[0], xs[1]))
+look_command = raw("look") + pp.Empty() + world_referent
+look_command.setParseAction(lambda xs : Look(xs[0]))
+
+command = ask_command | reply_command | reflect_command | view_command | gaze_command | move_command | look_command
---
+++ world.py
@@ -0,0 +1,117 @@
+from utils import clear_screen
+
+class X(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return X(self.x + other.x, self.y + other.y)
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __repr__(self):
+        return "X({}, {})".format(self.x, self.y)
+
+def update(grid, cell, f):
+    return update_tuple(grid, cell.x, lambda t : update_tuple(t, cell.y, f))
+
+def update_tuple(t, i, f):
+    return t[:i] + (f(t[i]),) + t[i+1:]
+
+def empty_world(x, y, start=X(0, 0)):
+    grid = (((),) * y,) * x
+    grid = add(grid, start, "agent")
+    return grid, start, start
+
+def render(xs):
+    if len(xs) == 0:
+        return "empty"
+    else:
+        return " and ".join(xs)
+
+def main():
+    grid, agent, gaze = empty_world(5, 5)
+    grid = add(grid, X(2, 2), "wall")
+    world = grid, agent, gaze
+    message = ""
+    while True:
+        print_world(world)
+        if message: print(message)
+        s = input("<<< ")
+        diff = direction[s]
+        world, moved = move_person(world, diff)
+        message = "" if moved else "!"
+
+def render_small(xs):
+    if "wall" in xs:
+        return "#"
+    if "agent" in xs:
+        if "goal" in xs:
+            return "!"
+        else:
+            return "@"
+    if "goal" in xs:
+        return "?"
+    return "."
+
+def world_repr(world):
+    grid, _, _ = world
+    return "\n".join("".join(render_small(x) for x in r) for r in grid)
+
+def print_world(world):
+    clear_screen()
+    print(world_repr(world))
+
+def remove(grid, cell, x, ignore_absent=False):
+    def f(xs):
+        assert (x in xs) or ignore_absent
+        return tuple(y for y in xs if y != x)
+    return update(grid, cell, f)
+
+def add(grid, cell, x):
+    return update(grid, cell, lambda xs : xs + (x,))
+
+obstructs = {"agent":True, "wall":True, "goal":False}
+direction = {"up":X(-1, 0), "down":X(1, 0), "left":X(0, -1), "right":X(0, 1)}
+
+def access(grid, cell):
+    return grid[cell.x][cell.y]
+
+def passable(grid, cell):
+    return in_bounds(grid, cell) and not any(obstructs[x] for x in access(grid, cell))
+
+def in_bounds(grid, cell):
+    i,j = cell
+    N,M = len(grid), len(grid[0])
+    return i >= 0 and i < N and j >= 0 and j < M
+
+def move_person(world, diff):
+    if isinstance(diff, str):
+        diff = direction[diff]
+    grid, agent_xy, gaze_xy = world
+    target_xy = agent_xy + diff
+    can_move = passable(grid, target_xy)
+    if can_move:
+        grid = add(grid, target_xy, "agent")
+        grid = remove(grid, agent_xy, "agent")
+        return (grid, target_xy, gaze_xy), True
+    else:
+        return world, False
+
+def move_gaze(world, diff):
+    if isinstance(diff, str):
+        diff = direction[diff]
+    grid, agent_xy, gaze_xy = world
+    target_xy = gaze_xy + diff
+    can_move = in_bounds(grid, target_xy)
+    if can_move:
+        return (grid, agent_xy, target_xy), True
+    else:
+        return world, False
+
+def look(world):
+    grid, agent_xy, gaze_xy = world
+    return render(access(grid, gaze_xy))
