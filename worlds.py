0b5e2abde815cd19c099ed01e2172622f659041d
paulfchristiano <paulfchristiano@gmail.com>
2017-07-16 19:02:17 -0700

    added pushable blocks

--- worlds.py
+++ worlds.py
@@ -58,48 +58,74 @@ def render(xs):
 
 def default_world():
     grid = empty_grid()
-    goalx, goaly = randint(1, height-1), randint(1, width-1)
-    agentx, agenty = randint(1, height-1), randint(1, width-1)
-    grid = add(grid, X(agentx, agenty), "agent")
-    grid = add(grid, X(goalx, goaly), "goal")
-    for i in range(height):
-        for j in range(width):
-            if ((i != goalx or j != goaly) and (i != agentx or j != agenty)
-                    and (i > 1 or j > 1)):
-                if random() < 0.25:
-                    grid = add(grid, X(i, j), "wall")
-    return grid, X(agentx, agenty), None
+    def add_random(grid, item):
+        while True:
+            x, y = randint(0, height-1), randint(0, width-1)
+            loc = X(x, y)
+            contents = access(grid, loc)
+            if "block" not in contents and "agent" not in contents and "wall" not in contents and not "goal" in contents:
+                grid = add(grid, loc, item)
+                return grid, loc
+
+    grid, agent_xy = add_random(grid, "agent")
+    grid, _ = add_random(grid, "goal")
+    for i in range(int(0.1 * width * height)):
+        grid, _ = add_random(grid, "block")
+    for i in range(int(0.2 * width * height)):
+        grid, _ = add_random(grid, "wall")
+    return grid, agent_xy, None
 
 def main():
     world = default_world()
     message = ""
-    while True:
-        print_world(world)
-        if message: print(message)
-        s = input("<<< ")
-        diff = direction[s]
-        world, moved = move_person(world, diff)
-        message = "" if moved else "!"
+    import term, termbox
+    with term.Terminal() as t:
+        while True:
+            t.clear()
+            print_world(world, t)
+            if message: t.print_line(message)
+            t.refresh()
+            ch, key = t.poll()
+            direction_name = {
+                termbox.KEY_ARROW_LEFT:'w',
+                termbox.KEY_ARROW_RIGHT:'e',
+                termbox.KEY_ARROW_DOWN:'s',
+                termbox.KEY_ARROW_UP:'n',
+            }.get(key, None)
+            if direction_name is not None:
+                direction = directions[direction_name]
+                world, moved = move_person(world, direction)
+            else:
+                moved = False
+            message = "" if moved else "!"
 
 def render_small(xs):
     if "wall" in xs:
         return "#"
     if "agent" in xs:
-        if "goal" in xs:
-            return "!"
-        else:
-            return "@"
+        return "§" if "goal" in xs else "@"
+    if "block" in xs:
+        return "≣" if "goal" in xs else "="
     if "goal" in xs:
-        return "?"
+        return "_"
     return "."
 
 def world_repr(world):
     grid, _, _ = world
     return "\n".join("".join(render_small(x) for x in r) for r in grid)
 
-def print_world(world):
-    clear_screen()
-    print(world_repr(world))
+def print_world(world, t=None):
+    if t is None:
+        clear_screen()
+    else:
+        t.clear()
+    lines = world_repr(world)
+    for line in lines.split("\n"):
+        if t is None:
+            print(line)
+        else:
+            t.print_line(line)
+
 
 def remove(grid, cell, x, ignore_absent=False):
     def f(xs):
@@ -110,8 +136,22 @@ def remove(grid, cell, x, ignore_absent=False):
 def add(grid, cell, x):
     return update(grid, cell, lambda xs : xs + (x,))
 
-obstructs = {"agent":True, "wall":True, "goal":False}
+def const(x):
+    return lambda *args, **kwargs : x
+
+def is_pushable(grid, cell, direction):
+    return passable(grid, cell+direction, direction)
+
+def push_block(grid, cell, direction):
+    grid = push(grid, cell+direction, direction)
+    grid = remove(grid, cell, "block")
+    grid = add(grid, cell+direction, "block")
+    return grid
+
+passable_test = {"agent": const(False), "wall": const(False), "goal": const(True), "block":is_pushable}
+pushers = {"block":push_block}
 directions = {"north":X(-1, 0), "south":X(1, 0), "west":X(0, -1), "east":X(0, 1)}
+for k in list(directions.keys()): directions[k[0]] = directions[k]
 
 def access(grid, cell):
     if cell.is_valid():
@@ -119,21 +159,30 @@ def access(grid, cell):
     else:
         return None
 
-def passable(grid, cell):
-    return in_bounds(grid, cell) and not any(obstructs[x] for x in access(grid, cell))
+def passable(grid, cell, direction):
+    if not in_bounds(grid, cell):
+        return False
+    return  all(passable_test[x](grid, cell, direction) for x in access(grid, cell))
 
 def in_bounds(grid, cell):
     i,j = cell
     N,M = len(grid), len(grid[0])
     return i >= 0 and i < N and j >= 0 and j < M
 
-def move_person(world, diff):
-    if isinstance(diff, str):
-        diff = directions[diff]
+def push(grid, cell, direction):
+    for x in access(grid, cell):
+        if x in pushers:
+            grid = pushers[x](grid, cell, direction)
+    return grid
+
+def move_person(world, direction):
+    if isinstance(direction, str):
+        direction = directions[direction]
     grid, agent_xy, previous = world
-    target_xy = agent_xy + diff
-    can_move = passable(grid, target_xy)
+    target_xy = agent_xy + direction
+    can_move = passable(grid, target_xy, direction)
     if can_move:
+        grid = push(grid, target_xy, direction)
         grid = add(grid, target_xy, "agent")
         grid = remove(grid, agent_xy, "agent")
         return (grid, target_xy, world), True
